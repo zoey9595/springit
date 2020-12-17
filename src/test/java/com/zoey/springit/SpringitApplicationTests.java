9e75f9faf7b2da0e3e8999ecff5ee062ef640815
Yuqing Zhai <u6865190@anu.edu.au>
2020-12-17 16:45:28 +1100

    Initial commit for our spring boot reddit clone!

---
+++ src/test/java/com/zoey/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zoey.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
