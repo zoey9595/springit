44de166b0b3696265363c7a951ac8fd44fbfe8cb
Yuqing Zhai <u6865190@anu.edu.au>
2020-12-17 21:14:29 +1100

    Created our entities and refactored using Project Lombok

---
+++ src/main/java/com/zoey/springit/domain/Comment.java
@@ -0,0 +1,19 @@
+package com.zoey.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String body;
+}
---
+++ src/main/java/com/zoey/springit/domain/Vote.java
@@ -0,0 +1,19 @@
+package com.zoey.springit.domain;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Vote {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private int vote;
+}
