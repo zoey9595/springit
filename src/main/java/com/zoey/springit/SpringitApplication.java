d195c5376d1d2416a0553cdcf12ca9114432f69f
Yuqing Zhai <u6865190@anu.edu.au>
2020-12-18 15:42:32 +1100

    Updated application properties with some basic h2 & datasource setup

--- src/main/java/com/zoey/springit/SpringitApplication.java
+++ src/main/java/com/zoey/springit/SpringitApplication.java
@@ -1,10 +1,14 @@
 package com.zoey.springit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringitApplication {
+
+    private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
 
     public static void main(String[] args) {
         SpringApplication.run(SpringitApplication.class, args);
