d0f45f1daaf0e6006f9cdfdfb0fdadbd9a6a8a8f
Yuqing Zhai <u6865190@anu.edu.au>
2020-12-20 19:19:51 +1100

    Auditing configuration

--- src/main/java/com/zoey/springit/SpringitApplication.java
+++ src/main/java/com/zoey/springit/SpringitApplication.java
@@ -6,10 +6,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableJpaAuditing
 public class SpringitApplication {
 
     private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
