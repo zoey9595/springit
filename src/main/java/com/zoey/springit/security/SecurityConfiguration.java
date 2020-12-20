fa54b8bbc76da08ecc62b906a5cc58e6b5cd22bc
Yuqing Zhai <u6865190@anu.edu.au>
2020-12-20 19:31:24 +1100

    H2 Console security configuration

--- src/main/java/com/zoey/springit/security/SecurityConfiguration.java
+++ src/main/java/com/zoey/springit/security/SecurityConfiguration.java
@@ -1,5 +1,6 @@
 package com.zoey.springit.security;
 
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -19,11 +20,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
+            .authorizeRequests()
+                .requestMatchers(EndpointRequest.to("info")).permitAll()
+                .requestMatchers(EndpointRequest.toAnyEndpoint()).hasRole("ADMIN")
+                .antMatchers("/actuator/").hasRole("ACTUATOR")
                 .antMatchers("/").permitAll()
                 .antMatchers("/link/submit").hasRole("USER")
-                .and()
-                .formLogin();
+                .antMatchers("/h2-console/**").permitAll()
+            .and()
+            .formLogin()
+            .and()
+                .csrf().disable()
+                .headers().frameOptions().disable();
     }
 
     @Override
