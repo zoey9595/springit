bce328e53b77b64937a8376af1244687c1498b29
Yuqing Zhai <u6865190@anu.edu.au>
2020-12-21 20:11:18 +1100

    Added transactional features

--- src/main/java/com/zoey/springit/SpringitApplication.java
+++ src/main/java/com/zoey/springit/SpringitApplication.java
@@ -6,9 +6,11 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class SpringitApplication {
 
     private static final Logger log = LoggerFactory.getLogger(SpringitApplication.class);
--- src/main/java/com/zoey/springit/service/UserService.java
+++ src/main/java/com/zoey/springit/service/UserService.java
@@ -6,6 +6,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
+
 @Service
 public class UserService {
 
@@ -25,4 +27,11 @@ public class UserService {
         userRepository.save(user);
     }
 
+    @Transactional
+    public void saveUsers(User... users) {
+        for (User user : users) {
+            logger.info("Saving User: " + user.getEmail());
+            userRepository.save(user);
+        }
+    }
 }
