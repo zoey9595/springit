b52e3f27ef8c3dcd37a876b189b341d7f3610264
Yuqing Zhai <u6865190@anu.edu.au>
2020-12-21 20:50:06 +1100

    Register function part 1

--- src/main/java/com/zoey/springit/controller/AuthController.java
+++ src/main/java/com/zoey/springit/controller/AuthController.java
@@ -1,10 +1,17 @@
 package com.zoey.springit.controller;
 
+import com.zoey.springit.domain.User;
 import com.zoey.springit.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
 
 @Controller
 public class AuthController {
@@ -28,7 +35,25 @@ public class AuthController {
     }
 
     @GetMapping("/register")
-    public String register() {
+    public String register(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("success", false);
         return "auth/register";
+    }
+
+    @PostMapping("/register")
+    public String registerNewUser(@Valid User user, BindingResult bindingResult, Model model, RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            logger.info("Validation errors were found while registering a new user");
+            model.addAttribute("user", user);
+            model.addAttribute("validationErrors", bindingResult.getAllErrors());
+            return "auth/register";
+        } else {
+            User newUser = userService.register(user);
+            redirectAttributes
+                    .addAttribute("id", newUser.getId())
+                    .addFlashAttribute("success", true);
+            return "redirect:/register";
+        }
     }
 }
